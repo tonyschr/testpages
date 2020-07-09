b027cd086e24892c523ce39a2d0e828729fc8a6d
Tony Schreiner <tonyschr@outlook.com>
2020-07-09 14:22:47 -0600

    Text to speech sample pages

---
+++ WordListHearingTest/TextToSpeech.js
@@ -0,0 +1,49 @@
+var g_selectedVoice = 0;
+var g_selectedPitch = 1;
+var g_selectedRate = 0.8;
+
+function populateVoices(id) {
+  voicesElement = document.getElementById(id);
+
+  var allVoices = speechSynthesis.getVoices();
+
+  var englishVoices = allVoices.filter(function(value, index, arr){ return value.name.includes("English"); });
+
+  for (var i = 0; i < englishVoices.length; i++) {
+    var option = document.createElement("option");
+    option.text = englishVoices[i].name;
+    voicesElement.add(option);
+  }
+}
+
+function onVoiceSelected(name) {
+  var voices = speechSynthesis.getVoices();
+
+  for (var i = 0; i < voices.length; i++) {
+    if (voices[i].name === name) {
+      g_selectedVoice = i;
+      break;
+    }
+  }
+}
+
+function onPitchSelected(value) {
+  g_selectedPitch = parseFloat(value);
+}
+
+function onRateSelected(value) {
+  g_selectedRate = parseFloat(value);
+}
+
+function readText(text) {
+  var utterance = new SpeechSynthesisUtterance();
+  utterance.text = text;
+
+  // Configure the speech characteristics.
+  utterance.pitch = g_selectedPitch;
+  utterance.rate = g_selectedRate;
+  utterance.volume = 1;
+  utterance.voice = speechSynthesis.getVoices()[g_selectedVoice];
+
+  speechSynthesis.speak(utterance);
+}
